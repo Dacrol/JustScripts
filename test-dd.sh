84533171b2331d716f25c902707796b516878fd9
Dacrol (Jonas Ahrens) <jonas.ahrens@gmail.com>
2020-03-15 00:12:39 +0100

    Fix echo to drop_caches with sudo

--- test-dd.sh
+++ test-dd.sh
@@ -21,7 +21,7 @@ for bs in  48M 1k 2k 4k 8k 16k 32k 64k 128k 256k 512k 1M 2M 4M 8M 10M 12M 16M 24
 
 do
         echo "Testing block size  = $bs"
-        [ -e /proc/sys/vm/drop_caches ] && echo 3 > /proc/sys/vm/drop_caches
+        [ -e /proc/sys/vm/drop_caches ] && bash -c "echo 3 > /proc/sys/vm/drop_caches"
         dd if=test_dd_infile of=test_dd_outfile bs=$bs
         echo ""
 done
