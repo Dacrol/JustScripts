ea23b06c25ffb529d45178c192b38ce263410a69
Dacrol (Jonas Ahrens) <jonas.ahrens@gmail.com>
2020-03-14 23:36:25 +0100

    Use random data

--- test-dd.sh
+++ test-dd.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-#
-#create a file to work with
-#
 
-set -ex
+set -e
 
 echo "creating a file to work with"
-dd if=/dev/zero of=/var/tmp/infile count=1175000
+#dd if=/dev/zero of=/var/tmp/infile count=1175000
+head -c 512M </dev/urandom >/var/tmp/infile
 
-for bs in  1k 2k 4k 8k 16k 32k 64k 128k 256k 512k 1M 2M 4M 8M 12M 16M 24M 48M 96M 
+for bs in  48M 1k 2k 4k 8k 16k 32k 64k 128k 256k 512k 1M 2M 4M 8M 10M 12M 16M 24M
 
 do
         echo "Testing block size  = $bs"
