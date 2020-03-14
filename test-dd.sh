8f67efdff362076b2c07d6ffe401fb419fb2073d
Dacrol (Jonas Ahrens) <jonas.ahrens@gmail.com>
2020-03-14 23:27:33 +0100

    Add dd testing script

---
+++ test-dd.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+#create a file to work with
+#
+
+set -ex
+
+echo "creating a file to work with"
+dd if=/dev/zero of=/var/tmp/infile count=1175000
+
+for bs in  1k 2k 4k 8k 16k 32k 64k 128k 256k 512k 1M 2M 4M 8M 12M 16M 24M 48M 96M 
+
+do
+        echo "Testing block size  = $bs"
+        dd if=/var/tmp/infile of=/var/tmp/outfile bs=$bs
+        echo ""
+done
+rm /var/tmp/infile /var/tmp/outfile
