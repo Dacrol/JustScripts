cbbaa314812b07cf9ffedcfa32139085f23f51df
Dacrol (Jonas Ahrens) <jonas.ahrens@gmail.com>
2020-03-14 23:50:20 +0100

    Use current directory files

--- test-dd.sh
+++ test-dd.sh
@@ -2,15 +2,22 @@
 
 set -e
 
-echo "creating a file to work with"
-#dd if=/dev/zero of=/var/tmp/infile count=1175000
-head -c 512M </dev/urandom >/var/tmp/infile
+{
+if [[ -f test_dd_infile || -f test_dd_outfile ]]; then
+    echo "File exists! Exiting."
+    exit 1
+fi
+}
 
-for bs in  48M 1k 2k 4k 8k 16k 32k 64k 128k 256k 512k 1M 2M 4M 8M 10M 12M 16M 24M
+echo "Creating infile"
+#dd if=/dev/zero of=/var/tmp/infile count=1175000
+head -c 512M </dev/urandom >test_dd_infile
+
+for bs in  48M 1k 2k 4k 8k 16k 32k 64k 128k 256k 512k 1M 2M 4M 8M 10M 12M 16M 24M 48M 96M
 
 do
         echo "Testing block size  = $bs"
-        dd if=/var/tmp/infile of=/var/tmp/outfile bs=$bs
+        dd if=test_dd_infile of=test_dd_outfile bs=$bs
         echo ""
 done
-rm /var/tmp/infile /var/tmp/outfile
+rm test_dd_infile test_dd_outfile
